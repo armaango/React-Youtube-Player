516ffa27c91edba9e40e8edea6dd12a22fe15e24
armaango <armaan.goyal@gmail.com>
2017-02-14 18:01:11 -0800

    linked the search bar to update videos

--- src/index.js
+++ src/index.js
@@ -19,8 +19,11 @@ class App extends Component {
             videos: [],
             selectedVideo: null
          };
+         this.videoSearch('surboards');
+    }
 
-        YTSearch({key: API_KEY,term: 'surboards'}, (videos) => {
+    videoSearch(term) {
+        YTSearch({key: API_KEY,term: term}, (videos) => {
             this.setState( { 
                 videos:videos,
                 selectedVideo:videos[0] 
@@ -31,7 +34,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})}
                 videos={this.state.videos} />
