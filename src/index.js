68fd5523ec26a034bb483e549722051da7248f54
armaango <armaan.goyal@gmail.com>
2017-02-14 17:52:09 -0800

    Added CSS styling

--- src/index.js
+++ src/index.js
@@ -14,9 +14,17 @@ const API_KEY = 'AIzaSyD5bWyBHa3M8pxkwnfVMRyHQX7swNjBerg';
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = { videos: [] };
+
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+         };
+
         YTSearch({key: API_KEY,term: 'surboards'}, (videos) => {
-            this.setState( { videos });
+            this.setState( { 
+                videos:videos,
+                selectedVideo:videos[0] 
+            });
         });
     }
 
@@ -24,8 +32,9 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
-                <VideoDetail video={this.state.videos[0]} />
-                <VideoList videos={this.state.videos} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                videos={this.state.videos} />
             </div>
         );
     }
